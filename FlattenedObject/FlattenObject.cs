222801654c9a98ecba8d9e28f3e944c89b8df084
rebeca robbins <rebecarobbins94@gmail.com>
2019-07-28 17:22:54 +0100

    First version of flattening complex data

---
+++ FlattenedObject/FlattenObject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlattenedObject
+{
+    public class FlattenObject
+    {
+        private static List<Tuple<string, object>> _flatternedObject;
+
+        private FlattenObject()
+        {
+            
+        }
+
+        public static List<Tuple<string, object>> Flattern<T>(T complexObject)
+        {
+            _flatternedObject = new List<Tuple<string, object>>();
+            var flatternResults = DoFlatterning(complexObject);
+            return _flatternedObject;
+        }
+
+
+        private static List<Tuple<string, object>> DoFlatterning<T> (T complexObject)
+        {
+            //for non anonymous object
+            //var filteredProperties = complexObject.GetType().GetProperties().Where(x => x.CanWrite);
+            var filteredProperties = complexObject.GetType().GetProperties();
+
+            foreach (var property in filteredProperties)
+            {
+                var propertyName = property.Name;
+                var propertyValue = property.GetValue(complexObject, null);
+
+                if(propertyValue != null)
+                {
+                    if(propertyValue.GetType().IsValueType || propertyValue is string)
+                    {
+                        _flatternedObject.Add(new Tuple<string, object>(propertyName, propertyValue));
+                    }
+                    else if (propertyValue is IEnumerable enumerable)
+                    {
+                        
+                        //loop through each item and flatten 
+                        foreach (var item in enumerable)
+                        {
+                            var results = DoFlatterning(item);
+                        }
+                    }
+                    else //is complex item
+                    {
+                        var results = DoFlatterning(propertyValue);
+                    }
+                }
+
+            }
+            
+            return _flatternedObject;
+        }
+
+
+    }
+}
---
+++ FlattenedObject/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace FlattenedObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            var obj1 = new
+            {
+                CompanyName = "Zoopla",
+                CompanyAddress =
+                new
+                {
+                    Number = 123,
+                    Street = "buxton street",
+                    Town = "London",
+                    PostCode = "NW11 7FD"
+                },
+                Employees =
+                new[]{
+                    new{
+                        Name = "Adam", Surname = "Bobbins", Age = 55
+                    },
+                    new{
+                        Name = "Susan", Surname = "Moore", Age = 30
+                    }
+                }
+            };
+
+            var flatObj1 = FlattenObject.Flattern(obj1);
+
+
+            foreach(var prop in flatObj1)
+            {
+                Console.WriteLine("Property Name: " + prop.Item1);
+                Console.WriteLine("Property Value: " + prop.Item2);
+            }
+
+
+
+            var obj2 = new
+            {
+                CompanyName = "RightMove",
+                CompanyAddress =
+                new
+                {
+                    Number = 123,
+                    Street = "bleh street",
+                    Town = "London",
+                    PostCode = "NW11 7FD"
+                },
+                Employees =
+                new[]{
+                    new{
+                        Name = "Carl", Surname = "Evens", Age = 55
+                    },
+                    new{
+                        Name = "Lisa", Surname = "Jenkins", Age = 30
+                    }
+                }
+            };
+            var flatObj2 = FlattenObject.Flattern(obj2);
+
+
+            foreach (var prop in flatObj2)
+            {
+                Console.WriteLine("Property Name: " + prop.Item1);
+                Console.WriteLine("Property Value: " + prop.Item2);
+            }
+
+
+        }
+
+
+
+    }
+}
